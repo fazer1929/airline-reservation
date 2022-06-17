aba0d67ffb783cb2409a6737937a26f700c20096
Abhay Choudhary <acabhaychoudhary562@gmail.com>
2022-06-18 01:32:48 +0530

    AboutUs page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="" element={<Login />} />
         <Route exact path="/signup" element={<Register />} />
+        <Route exact path="/aboutus" element={<AboutUs />} />
       </Routes>
     </Router>
 
