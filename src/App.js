c294cbcebdfcce5b2417529b36d6c230f2c6af4d
fazer1929 <abhishekagrawal8888@gmail.com>
2022-06-18 00:59:16 +0530

    dashboard & navbar UI

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import UserDashboard from './pages/UserDashboard';
+import AdminDashboard from './pages/AdminDashboard';
+import NavigationBar from './components/NavigationBar';
 
 function App() {
   return (
     <Router>
+      <NavigationBar />
       <Routes>
-        <Route exact path="" element={<Login />} />
         <Route exact path="/signup" element={<Register />} />
+        <Route exact path="/admin" element={<AdminDashboard />} />
+        <Route exact path="/user" element={<UserDashboard />} />
+        <Route exact path="" element={<Login />} />
       </Routes>
     </Router>
 
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,44 @@
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+function NavigationBar() {
+    const AdminNav = () => {
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Airline Reservation</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/admin" > Add Airline</Nav.Link>
+                        <Nav.Link href="#features" >See Flights</Nav.Link>
+                        <Nav.Link href="#pricing" >Airline Ticket</Nav.Link>
+                        <Nav.Link href="#pricing" className='text-danger'>Logout</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        )
+    }
+
+    const UserNav = () => {
+        return (
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Airline Reservation</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/admin" > Search Flights</Nav.Link>
+                        <Nav.Link href="#features" >Airline Ticket</Nav.Link>
+                        <Nav.Link href="#pricing" >Ticket Status</Nav.Link>
+                        <Nav.Link href="#pricing" className='text-white'>About Us</Nav.Link>
+                        <Nav.Link href="#pricing" className='text-danger'>Logout</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        )
+    }
+    return <>
+        {AdminNav()}
+
+    </>
+}
+
+
+export default NavigationBar
